02a0bf9abc6677338be492a5a58a27a17c830c3e
MonicaBaltes <monica.barbaalba@yaoo.com>
2025-10-23 15:46:25 +0300

    Am actualizat fundalul, logo-ul și stilul site-uluiiiii

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    # Citim toate imaginile din folderul tablouri pentru galerie
+    path = os.path.join(app.static_folder, "images/tablouri")
+    images = [f"images/tablouri/{img}" for img in os.listdir(path) if img.lower().endswith(('.jpg', '.jpeg', '.png'))]
+    return render_template('index.html', images=images)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
